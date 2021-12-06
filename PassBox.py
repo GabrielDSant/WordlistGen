2df8b1a1958468775f5d82d9be0e9face3d0c215
gabriel lucas <gabrielbiel1597533@gmail.com>
2021-12-06 14:30:10 -0300

    é duro...

---
+++ PassBox.py
@@ -0,0 +1,26 @@
+import itertools
+import re
+import sys
+
+#primeiro nome
+nome = input('Primeiro nome:')
+#Sobrenome
+sobrenome = input('Sobrenome:')
+#Nickname
+nickname = input('Apelido/Nick:')
+#Aniversário
+aniversario = input('Aniversário:')
+#Nome Companheiro(a)
+nome_companheiro = input('Nome companheiro(a):')
+#Nick Companheiro(a)
+nick_companheiro = input('Nick/Apelido Companheiro(a)')
+#Aniversário Companheiro(a)
+aniversario_companheiro = input('Aniversário companheiro(a):')
+#Nome Filho(a)
+#Nick Filho(a)
+#Aniversário Filho(a)
+#Nome Pet
+#Time de Futebol
+
+#Deseja adicionar simbolos ?
+#Deseja adicionar numero aletorios ?
