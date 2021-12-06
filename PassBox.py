d07c204d35d9d1b87a86e8d70093ed64b4bfedce
gabriel lucas <gabrielbiel1597533@gmail.com>
2021-12-06 20:57:37 -0300

    Estrutura feita

--- PassBox.py
+++ PassBox.py
@@ -1,7 +1,7 @@
 import itertools
-import re
+import string
 import sys
-
+from typing import Sequence
 #primeiro nome
 nome = input('Primeiro nome:')
 #Sobrenome
@@ -10,17 +10,46 @@ sobrenome = input('Sobrenome:')
 nickname = input('Apelido/Nick:')
 #Aniversário
 aniversario = input('Aniversário:')
+#Time de Futebol
+time = input('Time:')
+
 #Nome Companheiro(a)
 nome_companheiro = input('Nome companheiro(a):')
 #Nick Companheiro(a)
 nick_companheiro = input('Nick/Apelido Companheiro(a)')
 #Aniversário Companheiro(a)
 aniversario_companheiro = input('Aniversário companheiro(a):')
+
 #Nome Filho(a)
+nome_filho = input('Nome do filho(a):')
 #Nick Filho(a)
+nick_filho = input('Nick/Apelido Filho(a):')
 #Aniversário Filho(a)
+aniversario_filho = input('Anviersário Filho(a):')
+
 #Nome Pet
-#Time de Futebol
+nome_pet = input('Nome do Pet:')
 
 #Deseja adicionar simbolos ?
+simbolos = string.punctuation
 #Deseja adicionar numero aletorios ?
+numeros = string.digits
+
+#Aonde criar arquivo
+Arquivo = input('Aonde criar arquivo:')
+
+def CriandoWordlist():
+    def Alvo():
+        pass
+
+    def Companheiro():
+        pass
+
+    def Filhos():
+
+        pass
+
+    yield
+#criar arquivo.txt
+output = open(Arquivo, "a")
+output.write(CriandoWordlist())
