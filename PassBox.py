ce75d0373b8a641294f38253bfd3ac7c7f3bbd30
gabriel lucas <gabrielbiel1597533@gmail.com>
2021-12-07 01:17:54 -0300

    listas Feitas

--- PassBox.py
+++ PassBox.py
@@ -1,7 +1,6 @@
-import itertools
+import datetime
 import string
 import sys
-from typing import Sequence
 #primeiro nome
 nome = input('Primeiro nome:')
 #Sobrenome
@@ -9,7 +8,8 @@ sobrenome = input('Sobrenome:')
 #Nickname
 nickname = input('Apelido/Nick:')
 #Aniversário
-aniversario = input('Aniversário:')
+data = input("data:")
+aniversario = datetime.datetime.strptime(data, "%d/%m/%Y")
 #Time de Futebol
 time = input('Time:')
 
@@ -18,14 +18,16 @@ nome_companheiro = input('Nome companheiro(a):')
 #Nick Companheiro(a)
 nick_companheiro = input('Nick/Apelido Companheiro(a)')
 #Aniversário Companheiro(a)
-aniversario_companheiro = input('Aniversário companheiro(a):')
+data_companheiro = input('Aniversário companheiro(a):')
+aniversario_companheiro = datetime.datetime.strptime(data_companheiro, "%d/%m/%Y")
 
 #Nome Filho(a)
 nome_filho = input('Nome do filho(a):')
 #Nick Filho(a)
 nick_filho = input('Nick/Apelido Filho(a):')
 #Aniversário Filho(a)
-aniversario_filho = input('Anviersário Filho(a):')
+data_filho = input('Aniversário Filho(a):')
+aniversario_filho = datetime.datetime.strptime(data_filho, "%d/%m/%Y")
 
 #Nome Pet
 nome_pet = input('Nome do Pet:')
@@ -38,18 +40,54 @@ numeros = string.digits
 #Aonde criar arquivo
 Arquivo = input('Aonde criar arquivo:')
 
+info_alvo = [
+    nome.lower(),
+    nome.capitalize(),
+    sobrenome.lower(),
+    sobrenome.capitalize(),
+    nickname.lower(),
+    nickname.capitalize(),
+    aniversario.day,
+    aniversario.month,
+    aniversario.year]
+
+info_companheiro = [
+    nome_companheiro.lower(),
+    nome_companheiro.capitalize(),
+    nick_companheiro.lower(),
+    nick_companheiro.capitalize(),
+    aniversario_companheiro.day,
+    aniversario_companheiro.month,
+    aniversario_companheiro.year,]
+
+info_filho = [
+    nome_filho.lower(),
+    nome_filho.capitalize(),
+    nick_filho.lower(),
+    nick_filho.capitalize,
+    aniversario_filho.day,
+    aniversario_filho.month,
+    aniversario_filho.year]
+
+info_pet = [
+    nome_pet.lower(), 
+    nome_pet.capitalize()]
 def CriandoWordlist():
-    def Alvo():
+    def alvo():
         pass
 
-    def Companheiro():
-        pass
-
-    def Filhos():
+    def companheiro():
 
         pass
 
-    yield
+    def filhos():
+
+        pass
+
 #criar arquivo.txt
-output = open(Arquivo, "a")
-output.write(CriandoWordlist())
+#output = open(Arquivo, "a")
+#output.write(CriandoWordlist)
+
+if __name__ == '__main__':
+    print(info_alvo)
+    pass
